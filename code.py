6fb90a59cae843ddd2fad34ef932c07ed8a22ea8
99061142 <99061142@mydavinci.nl>
2022-05-14 01:07:45 +0200

    Added the board + keyboard functionallity

--- code.py
+++ code.py
@@ -9,16 +9,39 @@ class Window(tk.Tk):
         super().__init__() # "self" gets changed to the tkinter module
 
         # window for the game
-        self.game_window = tk.Frame(self, bg=self._bg)
+        self.game_window_frame = tk.Frame(self, bg=self._bg)
         self.title("Lingo")
         self.configure(background=self._bg)
         self.geometry("1000x500")
+        self.board()
         self.keyboard() # Add the keyboard
+    
+    def board(self):
+        # Frame for the board 
+        board_frame = tk.Frame(self.game_window_frame, bg=self._bg, pady=25)
+        board_frame.pack()
         
+        # For every chance the user has to guess the word
+        for row in range(self._chances):
+            # Frame for the word row
+            board_row = tk.Frame(board_frame, bg=self._bg, pady=3)
+            board_row.grid()
+
+            guessed_row_word = []
+
+            # For every character inside the word
+            for col in range(len(self._word)):
+                # Let the user change the text inside the label
+                character_guess = tk.StringVar()
+                guessed_row_word.append(character_guess)
+
+                ttk.Label(board_row, textvariable=character_guess, font=("Helvetica 15"), anchor='center').grid(row=row, column=col, ipadx=15, ipady=10, padx=3) # Label that shows the key the user guessed
+
+            self._guessed_words.append(guessed_row_word)
 
     def keyboard(self):
         # Frame for the keyboard
-        keyboard_frame = tk.Frame(self.game_window, bg=self._bg)
+        keyboard_frame = tk.Frame(self.game_window_frame, bg=self._bg)
         keyboard_frame.pack()
     
         # Keys on the keyboard
@@ -32,7 +55,7 @@ class Window(tk.Tk):
         # For every row on the keyboard
         for row, row_keys in enumerate(keys):
             # Frame for the row on the keyboard
-            row_frame = tk.Frame(keyboard_frame, bg=self._bg)
+            row_frame = tk.Frame(keyboard_frame, bg=self._bg, pady=3)
             row_frame.grid()
 
             # Add the button for each key
@@ -44,20 +67,32 @@ class Window(tk.Tk):
                     text=key.upper(),
                     width=width,
                     bg='#888888',
-                    fg='#F0F0F0'
-                ).grid(row=row, column=col, padx=3, pady=3)
+                    fg='#F0F0F0',
+                    command= lambda key=key: self.key_pressed(key)
+                ).grid(row=row, column=col, padx=3)
 
     def start(self):
         # Show the game
-        self.game_window.pack()    
+        self.game_window_frame.pack()    
         self.mainloop()
 
 
 class Game(Window):
-    _word = RandomWords().get_random_word(hasDictionaryDef="true", minLength=4, maxLength=6)
+    _word = RandomWords().get_random_word(hasDictionaryDef="true", minLength=6, maxLength=6)
+    _chances = 6
+    _guessed_words = []
+    _guess = 0
 
     def start(self):
         super().start() # Starts the game
+
+    def key_pressed(self, key):
+        # For every character inside the row the user is on 
+        for i, guessed_key in enumerate(self._guessed_words[self._guess]):
+            # If the column is not already guessed (length of the word was not reached)
+            if(not guessed_key.get()):
+                self._guessed_words[self._guess][i].set(key) # Change the character inside the row to the key the user has guessed
+                break
 
 
 
