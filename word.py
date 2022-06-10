3d210870bfa811f4766e48eeea9d86c7b1107b1e
99061142 <99061142@mydavinci.nl>
2022-06-10 18:39:36 +0200

    Made own module to create / check the word

---
+++ word.py
@@ -0,0 +1,10 @@
+from random import choice
+
+def possible_words() -> list:
+    return open('words.txt','r').read().splitlines()
+
+def random_word() -> str:
+    return choice(possible_words())
+
+def real_word(word):
+    return word in possible_words()
