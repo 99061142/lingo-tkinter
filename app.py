2628d088af77ee63ac7cd30dbf59a02205df5b7d
99061142 <99061142@mydavinci.nl>
2022-06-12 17:27:02 +0200

    Get end screen class, and added restart function

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 from board import Board
 from keyboard import Keyboard
+from endScreen import endScreen
 
-class App(Keyboard, Board):
+class App(Keyboard, Board, endScreen):
     def __init__(self):
         super().__init__()
 
-    def start(self):
+    def start(self):    
         self.mainloop()
+
+
+
+    def restart(self):
+        self.new_game()
+        self.enable_binding_events()
+        self.enable_keyboard()
 
 
 
