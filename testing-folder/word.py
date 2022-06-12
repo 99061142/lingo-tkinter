1dd625a17ece686961a531a496941425893a49bd
99061142 <99061142@mydavinci.nl>
2022-06-12 03:33:11 +0200

    Add the word module file to the testing folder

---
+++ testing-folder/word.py
@@ -0,0 +1,10 @@
+from random import choice
+
+def possible_words() -> list:
+    return open('words.txt','r').read().splitlines()
+
+def random_word() -> str:
+    return choice(possible_words())
+
+def real_word(word) -> bool:
+    return ''.join(word) in possible_words()
