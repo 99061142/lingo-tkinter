72a8b70ccb521368f0bdf49c54acbc6fbd7707ee
99061142 <99061142@mydavinci.nl>
2022-06-12 04:06:08 +0200

    bind the character when the event gets handled

--- testing-folder/window.py
+++ testing-folder/window.py
@@ -14,13 +14,13 @@ class Window(tk.Tk, metaclass=Type):
     def __init__(self):
         super().__init__()
         # GUI keyboard characters / binding events for own keyboard
-        self.keys = [      
+        self.keyboard_keys = [      
             {'q': 'q', 'w': 'w', 'e': 'e', 'r': 'r', 't': 't', 'y': 'y', 'u': 'u' , 'i': 'i', 'o': 'o' , 'p': 'p'},
             {'a': 'a', 's': 's', 'd': 'd', 'f': 'f', 'g': 'g', 'h': 'h', 'j': 'j', 'k': 'k', 'l': 'l'},
             {'Enter': '<Return>', 'z': 'z', 'x': 'x', 'c': 'c', 'v': 'v', 'b': 'b', 'n': 'n', 'm': 'm', 'BackSpace': '<BackSpace>'}
         ]
-        self.keyboard_chars = [char for row in self.keys for char in row]
-        self.binding_events = [row.get(char) for row in self.keys for char in row]
+        self.keyboard_chars = [char for row in self.keyboard_keys for char in row]
+        self.binding_events = [row.get(char) for row in self.keyboard_keys for char in row]
         self.keyboard_buttons = {}
 
         self.window_config()
@@ -41,13 +41,18 @@ class Window(tk.Tk, metaclass=Type):
         if(len(event) == 1):
             self.bind(event.upper(), lambda event=event: print(event))
 
+    def binding_event_to_char(self, event):
+        
+        return self.keyboard_chars[self.binding_events.index(event)]
+
     def enable_binding_events(self):
         for event in self.binding_events:
-            self.bind(event, lambda event=event: print(event))
+            char = self.binding_event_to_char(event).lower()
+            self.bind(event, lambda event, char=char: print(char))
 
             # Bind the uppercase character too
             if(len(event) == 1):
-                self.bind(event.upper(), lambda event=event: print(event))
+                self.bind(event.upper(), lambda event, char=char: print(char))
     
     def disable_binding_event(self, event:str):
         self.unbind(event)
