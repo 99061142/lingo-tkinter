182b7216ac1fcd2b1d5ee7f891548458e9301493
99061142 <99061142@mydavinci.nl>
2022-06-13 12:27:31 +0200

    Function to create text / color for the end screen

--- classes/endScreen.py
+++ classes/endScreen.py
@@ -5,6 +5,16 @@ class endScreen(Window):
     def __init__(self):
         super().__init__()
 
+    def end_message(self):
+        message = "YOU WON!" if self.word_guessed() else "YOU LOST! \n The word was: " + self.word
+        color = self.green if self.word_guessed() else self.red
+
+        end_message = {
+            "message": message,
+            "color": color,
+        }
+        return end_message
+
     def end_frame(self):
         end_frame = tk.Frame(
             self.board_frame,
@@ -17,12 +27,14 @@ class endScreen(Window):
         return end_frame
 
     def end_label(self, end_frame):
+        end_message = self.end_message()
+        
         tk.Label(
             end_frame,
-            text="Game over",
+            text=end_message['message'],
             font=("Helvetica 15"),
             background=self.light_gray,
-            foreground=self.red,
+            foreground=end_message['color'],
         ).grid(pady=25)
 
     def end_button(self, end_frame):
